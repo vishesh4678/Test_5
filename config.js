d0457640c547daea9b6515722a5f229e9c09faa0
vishesh4678 <119757897+vishesh4678@users.noreply.github.com>
2023-04-08 14:21:24 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-      user: 'arigpgty',
-      host: 'mahmud.db.elephantsql.com',
-      database: 'arigpgty',
-      password: 'oC6bhHjxEvZCyijhAU-edkdrY8WK8ef7',
-      port: 5432,
-    },
-  };
