f4b3781e23a325d18892340df56bf84f37f3b3e1
vishesh4678 <119757897+vishesh4678@users.noreply.github.com>
2023-04-08 13:07:04 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+      user: 'krhypwqm',
+      host: 'suleiman.db.elephantsql.com',
+      database: 'krhypwqm',
+      password: '0s9wUh-JU1SkxoXswFTv_zGYlicaX2Pp',
+      port: 5432,
+    },
+  };
